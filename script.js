b146d47ca5ef95267727a0b75f01bbfbc617f4e3
coding98html <134041067+coding98html@users.noreply.github.com>
2023-10-07 09:58:57 +0100

    The End

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ paragraphElement.addEventListener('click', function() {
         console.error('An error occurred:', error);
     }
 });
+
+// To change the image to make it look animated
+
+window.onload = function() {
+    var clicked = false;
+
+    document.getElementById('skeleton-img').onclick = function() {
+        var d = new Date();
+        if (!clicked) {
+            this.src = 'skeleton2.jpg?' + d.getTime();
+            clicked = true;
+        } else {
+            this.src = 'skeleton.ico?' + d.getTime();
+            clicked = false;
+        }
+    }
+}
+
